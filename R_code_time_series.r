33b7d6e7ab2499d2a68de76e54fe58e79722bf68
laviniaangelucci <80822666+laviniaangelucci@users.noreply.github.com>
2021-04-07 13:16:53 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,5 +1,5 @@
 # time series analysis 
-#greenland increase of temperature 
+# greenland increase of temperature 
 # data and code from Emanuela Cosma 
 
 install.packages("raster")
@@ -7,3 +7,37 @@ install.packages("raster")
 library(raster)
 
 setwd("C:/lab/greenland")
+
+# we are using the RASTER function 
+
+lst_2000 <- raster("lst_2000.tif")
+plot(lst_2000)
+
+lst_2005 <- raster("lst_2005.tif")
+plot(lst_2005)
+
+lst_2010 <- raster("lst_2010.tif")
+plot(lst_2010)
+
+lst_2015 <- raster("lst_2015.tif")
+plot(lst_2015)
+
+# funzione PAR per plottare matrice di immagini 
+par(mfrow=c(2,2))
+lst_2000 <- raster("lst_2000.tif")
+plot(lst_2000)
+lst_2005 <- raster("lst_2005.tif")
+plot(lst_2005)
+lst_2010 <- raster("lst_2010.tif")
+plot(lst_2010)
+lst_2015 <- raster("lst_2015.tif")
+plot(lst_2015)
+
+# using LIST and PATTERN functions per fare una lista di files raster 
+rlist <- list.files(pattern="lst")
+
+# LAPPLY function using list e plottiamo l'immagine
+import <- lapply(rlist,raster)
+
+TGr <- stack(import)
+plot(TGr)
