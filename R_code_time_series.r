10c5694d0f12e612b5d4fbc81f4c83e89cf90e7e
laviniaangelucci <80822666+laviniaangelucci@users.noreply.github.com>
2021-04-09 10:52:48 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -41,3 +41,53 @@ import <- lapply(rlist,raster)
 
 TGr <- stack(import)
 plot(TGr)
+
+# DAY 2 
+
+library(rasterVis)
+library(raster)
+setwd("C:/lab/greenland") 
+
+# codice per importare immagini 
+rlist <- list.files(pattern="lst")
+rlist
+import <- lapply(rlist,raster)
+import
+TGr <- stack(import)
+TGr
+
+# funzione level plot 
+levelplot(TGr)
+
+# plottiamo una singola immagine attraverso il pacchetto "rgdal"
+install.packages("rgdal")
+levelplot(TGr$lst_2000)
+
+# usiamo la colorramppalette per riplottare l'immagine
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+levelplot(TGr, col.regions=cl)
+
+# utiliziamo la funzione names.attr e la funzione main 
+levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+levelplot(TGr,col.regions=cl, main="LST variation in time",
+          names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+
+# utiliziamo i dati MELT ricavati attraverso il satellite a microwave
+meltlist <- list.files(pattern="melt")
+melt_import <- lapply(meltlist,raster)
+melt <- stack(melt_import)
+melt
+
+# facciamo il levelplot del meltlist 
+levelplot(melt)
+
+# sottraiamo il primo dato al secondo associando un nome "melttime"
+melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
+
+# plottiamo attraverso colorramppalette 
+clb <- colorRampPalette(c("blue","white","red"))(100)
+plot(melt_amount, col=clb)
+
+# facciamo un levelplot dell'argomento (melt_amount) 
+levelplot(melt_amount, col.regions=clb)
+ 
