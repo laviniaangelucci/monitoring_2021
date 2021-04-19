aab874aca6ac6a8bea61c5bce4681b0a34df00c1
laviniaangelucci <80822666+laviniaangelucci@users.noreply.github.com>
2021-04-19 16:37:52 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -5,5 +5,5 @@ setwd("C:/lab/")
 
 library(knitr)
 
-# 
-stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+# generare report partendo da script .r
+stitch("R_code_time_series.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
