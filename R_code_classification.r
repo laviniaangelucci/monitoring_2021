c6eeede8c65f1345a44c8509cc12862e3f151b52
laviniaangelucci <80822666+laviniaangelucci@users.noreply.github.com>
2021-04-23 10:01:15 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,5 +1,7 @@
 # R_code_classification.r
 
+# DAY 1 SOLAR ORBITER
+
 # settiamo la working directory che stiamo utilizzando 
 setwd("C:/lab/")
 library(raster)
@@ -32,3 +34,31 @@ https://www.esa.int/ESA_Multimedia/Missions/Solar_Orbiter/(result_type)/images
 sun <- brick("sun.png") 
 sunc <- unsuperClass(sun, nClasses=20)
 plot(sun.png$map)
+
+# DAY 2 GRAND CANYON
+
+# settiamo la working directory 
+ setwd("C:/lab/")
+library(raster)
+
+# portiamo l'immagine del Grand Canyon all'interno di R attraverso la funzione "brick" 
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+
+# plottiamo l'immagine attraverso funzione "RGB" con stretch lineare e stretch "hist" che presenta uno "slope" più "ripido" rispetto allo stretching lineare
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+# installo RStoolbox 
+install.packages("RStoolbox")
+library(RStoolbox)
+
+# utiliziamo la funzione "unsuperClass" con n. 2 di classi per classificare l'immagine e plottiamo successivamente
+gcc2 <- unsuperClass(gc, nClasses=2)
+gcc2
+plot(gcc2$map)
+
+# utiliziamo la funzione di classificazione con n.4 di classi e plottiamo succesivamente 
+gcc4 <- unsuperClass(gc, nClasses=4)
+plot(gcc4$map)
+
+
